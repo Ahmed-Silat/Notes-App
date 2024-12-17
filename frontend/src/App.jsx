552de999f368fb9268a842a71b577dbf3ad19847
Muhammad Ahmed <ahmedsilat95@gmail.com>
2024-12-17 23:43:19 +0500

    project initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
+import Navbar from "./components/Navbar";
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
