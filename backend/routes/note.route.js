bedc5f4b9a16c0233c626732017a56887296872b
Muhammad Ahmed <ahmedsilat95@gmail.com>
2025-05-04 16:05:00 +0500

    note pinned feature api is created

--- backend/routes/note.route.js
+++ backend/routes/note.route.js
@@ -5,13 +5,15 @@ import {
   deleteNote,
   editNote,
   getAllNotes,
+  updateNotePinned,
 } from "../controller/note.controller.js";
 
 const router = express.Router();
 
 router.post("/add", verifyToken, addNote);
-router.post("/edit/:noteId", verifyToken, editNote);
+router.put("/edit/:noteId", verifyToken, editNote);
 router.get("/all", verifyToken, getAllNotes);
 router.delete("/delete/:noteId", verifyToken, deleteNote);
+router.put("/update-note-pinned/:noteId", verifyToken, updateNotePinned);
 
 export default router;
